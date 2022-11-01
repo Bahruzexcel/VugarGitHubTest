ba9888588770b0f346d919b4483f58f65da1bc12
Bahruz Gadirov <qedirovbehruz@gmail.com>
2022-11-01 16:12:30 -0400

    second commit

--- src/Java.java
+++ src/Java.java
@@ -2,5 +2,6 @@ public class Java {
     public static void main(String[] args) {
         System.out.println("Vugar test project");
         System.out.println("Bahruz ++");
+        System.out.println("Vugar");
     }
 }
