edeb73a0403fb955258f469b0f0260c2a7f02683
Vugar <vugar.pelc@gmail.com>
2022-11-16 09:49:44 -0500

    Added VNG

--- src/Java.java
+++ src/Java.java
@@ -1,10 +1,10 @@
 public class Java {
     public static void main(String[] args) {
         System.out.println("Vugar test project");
-        System.out.println("Bahruz ++");
+        System.out.println("Nurgul ++");
         System.out.println("Vugar");
         System.out.println("test");
-        System.out.println("Bahruz");
+        System.out.println("Nurgul");
         System.out.println("Vugar++");
         System.out.println("hello");
         System.out.println("new class");
